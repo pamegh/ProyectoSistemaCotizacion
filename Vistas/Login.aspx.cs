9c728759390c6addd0c0405e174e191938bc688c
Khaill <naomiariasbonilla2828@gmail.com>
2026-02-09 15:30:39 -0600

    Login de prueba creado

--- Vistas/Login.aspx.cs
+++ Vistas/Login.aspx.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace ProyectoSistemaCotizacion.Vistas
+using ProyectoSistemaCotizacion.Controladores;
+namespace SistemaCotizacionAPF.Vista
 {
 	public partial class Login : System.Web.UI.Page
 	{
