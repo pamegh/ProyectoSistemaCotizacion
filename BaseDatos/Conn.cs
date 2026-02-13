5d93e2046dbc3ced4bb597901963537597bf54b8
pamegh <hpamegh77@gmail.com>
2026-02-12 18:07:52 -0600

    Configuración de la base de datos y agraga modelos

---
+++ BaseDatos/Conn.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+using System.Configuration;  
+
+namespace VeterinariaWpfApp1.Bases_de_Datos
+{
+     class Conn
+    {
+        Conn()
+        {
+
+        }
+        public static string GetConnectionStrings()
+        {
+            string resultado = "";
+            ConnectionStringSettingsCollection settings =
+                ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                resultado = settings[0].ConnectionString; 
+            }
+            return resultado;
+        }
+    }
+}
